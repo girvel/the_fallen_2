e852a7c0803a3ae5528b3bdc7917747e34177283
girvel <widauka@ya.ru>
2026-04-29 11:59:14 +0500

    Implement the intro scene

--- level/rails.lua
+++ level/rails.lua
@@ -46,31 +46,38 @@ init_debug = function()
   State.player.base_abilities.cha = 20
 
   State.runner:extend {
-    demo_scene = cutscene.make {
+    intro = cutscene.make {
       enabled = true,
-      screenplay = "assets/screenplay/00_test.ms",
+      screenplay = "assets/screenplay/intro.ms",
       characters = {
         player = {},
-        ai_tester = {},
       },
 
       _run = function(self, ch, ps, sp)
+        api.fade_out(0):wait()
+        local fade_in = api.fade_in(1)
+        State.player:rotate(Vector.up)
+        State.player:animate("lying", false, true)
         sp:lines()
-
-        local promise, scene = State.runner:run_task(function()
-          api.travel_scripted(ch.ai_tester, ps.travel_to):wait()
-        end, "parent")
-
-        async.sleep(.5)
-        State.runner:cancel(scene, true)
-
-        promise, scene = State.runner:run_task(function()
-          async.sleep(5)
-          State.player.hp = 50
+        fade_in:wait()
+        State.player:animate()
+        local look_around = State.runner:run_task(function()
+          async.sleep(.5)
+          State.player:rotate(Vector.right)
+          async.sleep(.8)
+          State.player:rotate(Vector.left)
         end)
-        scene.save_flag = true
-        coroutine.yield()
-        api.autosave("test")
+
+        sp:lines()
+        look_around:wait()
+
+        sp:start_single_branch()
+          if State.player:saving_throw("wis", 13) then
+            sp:lines()
+          end
+        sp:finish_single_branch()
+
+        sp:lines()
       end,
     },
   }
